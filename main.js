df4f0c37cbb38043a9b3ccc760b347e829676b6b
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-14 16:20:57 +0200

    Milestone 2 - Finita con controllo minimodel testo

--- main.js
+++ main.js
@@ -9,18 +9,22 @@ createApp({
                 "Giocare",
                 "Imparare"
             ],
-            task : 0,
-            text : ""
+            task: 0,
+            text: ""
 
         }
     },
 
     methods: {
         done(indice) {
-            this.toDo.splice(indice);
+            this.toDo.splice(indice, 1);
         },
-        print(){
+        print() {
             console.log(this.text)
+            if (this.text.length > 3) {
+                this.toDo.push(this.text)
+                this.text = "";
+            }
         }
     }
 }).mount('.container-lista')
