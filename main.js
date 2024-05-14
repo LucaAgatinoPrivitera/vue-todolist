f6d7dc8a82315d06a8bfb9bae08c8e1ba6921c2b
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-14 15:56:55 +0200

    Milestone 2 iniziata ma aspetto ancora per la prima

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            toDo : [
+            toDo: [
                 "Fare i progetti pomeridiani",
                 "Continuare il videocorso di CS50",
                 "Giocare",
                 "Imparare"
-            ]
+            ],
+            task : 0
 
+        }
+    },
+
+    methods: {
+        done(indice) {
+            this.toDo.splice(indice);
         }
     }
 }).mount('.container-lista')
